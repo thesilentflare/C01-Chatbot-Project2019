c3437646611f19b3a177a5b305cb7cc62c34d8e0
Gavin Leung <gawain.leung@mail.utoronto.ca>
2019-06-19 14:06:51 -0400

    added react router

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/*<Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path = "/apply" component={Apply} />
+          <Route exact path = "/team" component={Team} />
+          <Route path = "/Student" component={Student} />
+          <Route path = "/form" component={Form} />
+        </Switch>
+      </Router>*/}
     </div>
   );
 }
