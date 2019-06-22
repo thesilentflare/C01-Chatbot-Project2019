5449605537e68b0894e625563c1993c9d884020d
Birathan <birathan.somasundaram@mail.utoronto.ca>
2019-06-22 19:14:02 -0400

    PROJ8-6 added more attributes to database tables

--- api/configs/database.js
+++ api/configs/database.js
@@ -5,27 +5,56 @@ const defaultConfig = config.development;
 const database = new Sequelize(defaultConfig);
 
 var User = database.define("user",{
-  username: {
-    type: Sequelize.STRING(20),
+  email: {
+    type: Sequelize.STRING(150),
     allowNull: false,
     unique: true
   },
+  firstname: {
+    type: Sequelize.STRING(30),
+    allowNull: false,
+  },
+  lastname: {
+    type: Sequelize.STRING(30),
+    allowNull: false,
+  },
+  gender: {
+    type: Sequelize.ENUM('M','F'),
+    allowNull: false
+  },
+  reason: {
+    type: Sequelize.STRING(250),
+    allowNull: false,
+  },
   password: {
-    type: Sequelize.STRING(20),
+    type: Sequelize.STRING(32),
     allowNull: false
   }  //just for now, can do salted hashes later for security
   //salt:
   //saltedHash:
 });
 
-database.define("index",{
+var Admin = database.define("admins",{
+  email: {
+    type: Sequelize.STRING(150),
+    allowNull: false,
+    unique: true
+  },
+  adminToken: {
+    type: Sequelize.STRING(10),
+    allowNull: false,
+  }
+});
+
+
+var Index = database.define("index",{
   indexName: {
     type: Sequelize.STRING(20),
     allowNull: false,
     unique: true
   },
-  url: {
-    type: Sequelize.STRING,
+  urls: {
+    type: Sequelize.ARRAY(Sequelize.STRING(200)),
     allowNull: false
   }
 });
