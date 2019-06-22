b1f5a6c9ddbfc16dba8107d8bcdfc9de3b849864
Birathan <34045539+Birathan@users.noreply.github.com>
2019-06-21 21:38:18 -0400

    PROJ8-6 setup the basic user and index tables in the database

--- api/configs/database.js
+++ api/configs/database.js
@@ -4,3 +4,35 @@ const defaultConfig = config.development;
 
 const database = new Sequelize(defaultConfig);
 
+var User = database.define("user",{
+  username: {
+    type: Sequelize.STRING(20),
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING(20),
+    allowNull: false
+  }  //just for now, can do salted hashes later for security
+  //salt:
+  //saltedHash:
+});
+
+database.define("index",{
+  indexName: {
+    type: Sequelize.STRING(20),
+    allowNull: false,
+    unique: true
+  },
+  url: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+/*database.sync().then(function() {
+  User.create({
+    username:"admin",
+    password:"admin"
+  });
+})*/
