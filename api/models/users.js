42e125c3db8f39cb49bff4c6ff569f299de4c9b8
Caleb <caleb.dsouza@mail.utoronto.ca>
2019-06-23 01:24:00 -0400

    PROJ8-66 - Added validation and admin secifications for user table

--- api/models/users.js
+++ api/models/users.js
@@ -1,13 +1,37 @@
+const Sequelize = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('author', {
         id: {
-          type: DataTypes.INTEGER,
+          type: Sequelize.INTEGER,
           primaryKey: true,
           autoIncrement: true
         },
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        email: DataTypes.STRING
+        firstName: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        lastName: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+          unique: true
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: false
+        },
+        salt: {
+          type: Sequelize.STRING(32),
+          allowNull: false
+        },
+        admin: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false
+        }
       },
       {
         freezeTableName: true,
