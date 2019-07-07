d13e8edd91fa4389032b2d39f2f21eb497a48c2c
Caleb <caleb.dsouza@mail.utoronto.ca>
2019-07-06 22:54:01 -0400

    PROJ8-84 Created inital Lucene Document creater class starter code

---
+++ apis/search/src/main/java/com/group8/search/models/Documenter.java
@@ -0,0 +1,29 @@
+package com.group8.search.models;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import org.apache.lucene.document.Document;
+
+class Documenter {
+    // Odcument object instance fields
+    private ResultSet resultSet;
+    
+    /**
+     * Create a Documenter object that generates Lucene Documents that are
+     * added to an Index
+     * 
+     */
+    public Documenter() {
+        
+    }
+    
+    public Document createDocument() {
+        
+    }
+
+    public ArrayList<Document> getDocuments() {
+        
+    }
+
+
+}
