401006400ccd1d44af35cb10cc7046abb04b99a7
Birathan <birathan.somasundaram@mail.utoronto.ca>
2019-07-06 20:58:57 -0400

    Revert "PROJ8-72,PROJ8-6 setup the data indexer with search and web crawling insertion"
    This reverts commit 428e02951a8c829d0ee37d5c6897c6e0eed0de02.

--- apis/login/routes/index.js
+++ apis/login/routes/index.js
@@ -1,7 +1,7 @@
 //Routes
 const login = require('./login.js');
 const signUp = require('./signup.js');
-const index = require('./indexer.js');
+
 /**
  * Add routes to given express app object
  * @param {object} app - api express app
@@ -10,7 +10,6 @@ const initRoutes = (app) => {
 	// set all the server things
 	app.use('/login', login);
 	app.use('/signUp', signUp);
-	app.use('/index', index);
 }
 
 module.exports = {
