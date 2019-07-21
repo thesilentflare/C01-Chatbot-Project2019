d54459417a510077717d4b49d1f64c9429f4f3a8
Caleb <caleb.dsouza@mail.utoronto.ca>
2019-07-20 23:39:39 -0400

    PROJ8-96 updated comments

--- apis/search/src/main/java/com/group8/search/models/ArticleDAO.java
+++ apis/search/src/main/java/com/group8/search/models/ArticleDAO.java
@@ -2,11 +2,44 @@ package com.group8.search.models;
 
 import java.util.List;
 
-interface ArticleDAO {
-    List<Article> findAll();
-    Article find(String name);
-    void create(Article article);
-    void update(Article article);
-    void delete(String name);
-    boolean doesExist(String name);
+import com.mysql.cj.jdbc.result.ResultSetMetaData;
+
+public interface ArticleDAO {
+    
+    /**
+     * Get a list of all the Atricles in the database
+     * @return List<Article> - all Articles in this database
+     */
+    public List<Article> findAll();
+
+    /**
+     * Get an Article with the given name
+     * @param name String - the name of the Article to be found
+     * @return Article - resulting Article related to the given name
+     */
+    public Article find(String name);
+
+    /**
+     * Create the given Article as entry in the database's article table
+     * @param article Article - the Article to be entered in the database
+     */
+    public void create(Article article);
+
+    /**
+     * Update the related database entry with the given Article
+     * @param article Article - the Article information to be updated
+     */
+    public void update(Article article);
+
+    /**
+     * Detele the database entry related to the given Article name
+     * @param name String - the name of the entry to be deleted
+     */
+    public void delete(String name);
+
+    /**
+     * Get the result set meta data about this related Article table
+     * @return ResultSetMetaData
+     */
+    public ResultSetMetaData getMetaData();
 }
